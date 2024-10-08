9eebfa9510a7a46f00ba9e938bdd9c636ab8106a
Diego Belini <drxbelini@gmail.com>
2024-10-08 15:19:15 -0300

    Desafio concluido

---
+++ Models/Reserva.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            if (hospedes.Count <= Suite.Capacidade)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                throw new ArgumentException($"\n\nNumero de hospedes é maior que capacidade \nHospedes:{hospedes.Count} \nCapacidade:{Suite.Capacidade}\n");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+            if (DiasReservados >= 10)
+            {
+                valor = DiasReservados * Suite.ValorDiaria ;
+                return valor = valor - valor * 0.10M;
+            }
+            else
+            {
+                return valor;
+            }
+            
+        }
+    }
+}
